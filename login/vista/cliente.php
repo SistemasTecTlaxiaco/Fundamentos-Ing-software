588245245bb6b68af43419036d7f2dbad28c5091
Eva155 <aguilar19eva@gmail.com>
2021-05-24 15:06:49 -0500

    Subiendo cambios

--- login/vista/cliente.php
+++ login/vista/cliente.php
@@ -1,7 +1,7 @@
 <?php
-session_start();
-if($_SESSION['tipo_usuario']==1
-){
+	session_start();
+	if($_SESSION['tipo_usuario']==1
+	){
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,54 +10,34 @@ if($_SESSION['tipo_usuario']==1
         <meta name="viewport" >
         <link rel="stylesheet" type="text/css" href="../css/style.css">
         <link rel="stylesheet" type="text/css" href="../css/CSS.CSS">
-        <title>cliente</title>
-        
-
+        <title>Presatador</title>
 </head>
 <body>
-
-    
-<a href='../controlador/inicio_login.php'>Cerrar session</a>
-
-    <div id="pagewrap">
-    <header id="header">
-     
-       <hgroup>
-           <h1>Bienvenido Cliente</h1>
-       </hgroup>
-
-       <nav>
-        <ul id="main-nav" class="clearfix">
-
-            <li><a href=' ' target="iframea">PRINCIPAL</a></li>
-            
-        </ul>
-        
-       </nav>
-
-       <form id="searchform">
-        <input type="search" id="s" placeholder="Search">
-       </form>
-</header>
-
-	<div id="content">
+	<a href='../controlador/inicio_login.php'>Cerrar session</a>
+		<div id="pagewrap">
+		<header id="header">
+			<hgroup>
+				<div class="header-logo"><a href="#"><img src="../img/serv1.jpg" aling="right" width="10%" height="10%"></img></a></h1>
+				<h1 id="site-logo"><a href="#">BIENVENIDO A PRESTADORES DE SERVICIO</a></h1>
+			</hgroup>
+			<nav>
+				<ul id="main-nav" class="clearfix">
+					<li><a href=' ' target="iframea">PRINCIPAL</a></li>
+					<li><a href="../paginas/nosotros.html" target="iframea">NOSOTROS</a></li>
+					<li><a href="../paginas/nosotros.html" target="iframea">SERVICIOS</a></li>
+				</ul>
+			</nav>
+			<form id="searchform">
+				<input type="search" id="s" placeholder="Search">
+			</form>
+		</header>
+		<div id="content">
 		<h2>Contenido</h2>
 		<<img src="../img/loginprestadores.jpg" alt="" >
 	</div>
-
-	
-	
-	
-
-
- 
 </html>
 </body>
-
-
 <?php
-
-
 }
 else{
     header('Location: ../vista/login.php');
