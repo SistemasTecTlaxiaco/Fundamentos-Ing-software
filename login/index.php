bd25705ad4fdc66aa6df7ed8d6fac9e9f35ce545
Graciela2000 <chequi-2000+@hotmail.com>
2021-05-24 00:18:42 -0500

    subiendo archivo index

---
+++ login/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta> 
+<charset="utf-8">
+<title>Bienvenido Prestadores de servicios EGAL</title>
+<link rel="stylesheet" type="text/css" href="./css/estilosreg.css">
+  </head>
+
+  <body>
+  <form method="post">
+  <h1>Bienvenido a prestadores de servicio EGAL </h1>
+  <a href="vista/login.php"> Ingresar</a> o
+  <a href="vista/registro.php">Registrate</a>
+  </form>
+  </body>
+</html>
