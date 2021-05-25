cdf8f0e9c2ce1ac031ff98b530fd91639180599d
Graciela2000 <chequi-2000+@hotmail.com>
2021-05-25 13:00:12 -0500

    Actualizando Usuario

--- login/modelo/Usuario.php
+++ login/modelo/Usuario.php
@@ -1,21 +1,19 @@
+<?php 
+    include_once 'Conexion.php';
+    class User extends Database{
 
-<?php
-include_once 'Conexion.php';
-class Usuario{
-    var $objetos;
-    public function __construct(){
-        $db = new Conexion();
-        $this->acceso = $db->pdo;
+        public function getUser($username, $password){
+            $sql = "SELECT * FROM usuario WHERE Email= '$username' AND contrasena_u='$password'";
+
+            $result = $this->connect()->query($sql);
+
+            $numRows = $result->num_rows;
+            if($numRows == 1){
+                return true;
+            }
+
+            return false;
+        }
+
     }
-    function Loguearse($dni,$pass){
-        $sql="SELECT * FROM usuario inner join usuario_tipo on tipo_usuario=id_tipousuario where  Email=:dni and contrasena_u=:pass";
-
-
-        $query = $this->acceso->prepare($sql);
-        $query->execute(array(':dni' => $dni,':pass'=>$pass));
-        $this->objetos = $query->fetchAll();
-        return $this->objetos;
-       
-    }
-}
-?>
+   ?> 
