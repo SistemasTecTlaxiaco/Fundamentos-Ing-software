360b712878046d2045e2d48417094c8a4d73450b
lizzeth99 <lizzeth.kiroz@gmail.com>
2021-05-26 10:13:29 -0500

    subiendo cambios

---
+++ login/controlador/index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="css/estilo.css">
+	</head>
+	<body>
+		<div id="principal">
+			<header>
+				<div id="logo">
+					<h2 class="titulo">"EGAL"</h2>
+					<p class="sub-titulo">Prestadores De Servicios</p>
+					
+				</div>
+
+				<nav class="menu">
+					
+				</nav>
+			</header>
+
+			<section id="slide">
+				<img src="img/slide.jpg" width="900" height="250">
+			</section>
+
+			<section id="categorias">
+				<section class="uno">
+					<img src="img/disenio_web.png" width="250" height="150" >
+					<h2>Auxiliar de mantenimiento de obra civil</h2>
+					<p class="parrafo">
+						<font color="#000000 ">
+						Seguimiento a mantenimiento preventivos y activos, a través del monitoreo.
+ comprometidos a brindar igualdad de oportunidades de desarrollo.
+ <p>Ubicados en la Ciudad de Mexico.</p>
+
+
+					</p>
+					<a href="contacto.html" class="leer-mas">mas informacion</a><br><br>
+				</section>
+				<section class="uno dos">
+					<img src="img/programacion.png" width="250" height="150" >
+					<h2>Programador</h2>
+					<p class="parrafo">
+						<font color="#000000 ">
+						El programador, tiene como cometido analizar un problema y describirlo con el propósito de ser solucionado mediante un sistema de información.
+                        El programador, cuya única función consistíra en trasladar 
+						en la computadora. Dichas especificaciones se recogen en un documento
+							<p>Ubicado en la ciudad de Tlaxiaco Oaxaca</p>
+						
+						
+
+  			</p>
+					<a href="contacto.html" class="leer-mas">mas informacion</a><br><br>
+				</section>
+				<section class="uno">
+					<img src="img/descarga.jpg" width="250" height="150" >
+					<h2>Maestro de albañileria,pinturas y pisos </h2>
+					<p class="parrafo">
+						<font color="#000000 ">
+							Hacer seguimiento e informar sobre la progresión y el desarrollo 
+							de los proyectos de construcción, garantizando el respeto del tiempo 
+							de gestión.
+							Actualizar y regular la disposición presupuestaria por proyecto, 
+							controlando los ingresos y egresos.
+							Visitar las obras para supervisar el trabajo de los 
+							subcontratistas.
+							<p>Ubicado en la ciudad de Tlaxiaco Oaxaca</p>
+					</p>
+					<a href="contacto.html" class="leer-mas">mas informacion</a><br><br>
+				</section>
+			</section><!--/categorias-->
+
+			
+					<section id="categorias">
+						<section class="uno">
+							<img src="img/cocinera.jpg" width="250" height="150" >
+							<h2>Cocinera</h2>
+							<p class="parrafo">
+								<font color="#000000 ">
+									Las funciones en la cocina se categorizan en función 
+									de los conocimientos y las especialidades.
+		 <p>Ubicados en la ciudad de Oaxaca.</p>
+		</p>
+		<a href="contacto.html" class="leer-mas">Mas informacion</a><br><br>
+	</section>
+	</p>
+
+	<section id="categorias">
+	<section class="uno">
+	<img src="img/limpieza.jpg" width="250" height="150" >
+	<h2>Auxiliar De Limpieza Y Mantenimiento </h2>
+	<p class="parrafo">
+	<font color="#000000 ">
+		Limpieza de suelos, muebles y cristales. Generalmente esto se basa en barrer, fregar, quitar el polvo, entre otros.
+		Vaciado de papeleras o eliminación de residuos.
+		Reposición de materiales, como papel higiénico o servilletas.
+		Mantenimiento de equipos.
+				 <p>Ubicados en la ciudad de Tlaxiaco Oaxaca.</p>
+				</p>
+				<a href="contacto.html" class="leer-mas">Mas informacion</a><br><br>
+			</section>
+			</section><!--/info-->
+			<footer>
+				
+			</footer>
+
+			
+		</div>
+
+	</body>
+</html>
