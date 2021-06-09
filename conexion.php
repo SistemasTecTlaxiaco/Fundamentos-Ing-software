9b13f0db84a8fb3f54a2d7652e62df7b0066cb70
Anthony4523 <hernandezantonio4523@gmail.com>
2021-06-09 15:59:13 -0500

    Modificando archivo de conexión

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php 
 
-	$host = 'db4free.net';
-	$user = 'useregal123';
-	$password = 'prestadoresEGAL_2021';
-	$db = 'dbprestadores123';
+	$host = 'prestadoresdeservicio.c0nt5wef8jyz.us-east-2.rds.amazonaws.com';
+	$user = 'admin';
+	$password = 'prestadores_2021';
+	$db = 'proyectoEgal';
 
 	$conection =@mysqli_connect($host,$user,$password,$db);
 
