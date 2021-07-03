e0b764b811e59f09a76a59fa8445dde7eaa30492
Graciela2000 <chequi-2000+@hotmail.com>
2021-07-03 15:31:30 -0500

    actualizaciones

--- sistema/index.php
+++ sistema/index.php
@@ -1,56 +1,85 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<?php include "includes/scripts.php" ?>
+	
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta charset="UTF-8">
-	<?php include "includes/scripts.php" ?>
+	
 	<title>Prestadores de Servicio EGAL</title>
+	<link rel="stylesheet" href="styles.css">
+  
 </head>
+
 <body>
-	<?php include "includes/header.php" ?>
-	<section id="container">
-		<div class="form_register">
-			<form action="" method="post">
-				<h1>Ofertas de Trabajo</h1>
-				<div class="imagen">
-				<h4>Albañil</h4>
-				<li>Direccion: Ocotlán de Morelos, Oax</li>
-				<li>Precio: $8,000 - $10,0000 mensuales</li>
-				<img class="imgn" src="../img/albañil.jpg">
-			</div>
-			<div class="imagen">
-				<h4>Carpintero</h4>
-				<li>Dirección: Oaxaca de Juarez, Oax</li>
-				<li>Precio: $10,000 - $12,0000 mensuales</li>
-				<img class="imgn" src="../img/carpinteria.jpg">
-			</div>
-			<div class="imagen">
-				<h4>Plomero</h4>
-				<li>Direccion: 5 de mayo Oaxaca de Juarez</li>
-				<li>Precio: $7,000 - $8,0000 mensuales</li>
-				<img class="imgn" src="../img/plomero.jpg">
-			</div>
-			<div class="imagen">
-				<h4>Auxiliar de limpieza y mantenimiento</h4>
-				<li>Direccion: Heroica,Cd. de Tlaxiaco, Oax</li>
-				<li>Precio: $5,000 - $5,500 mensuales</li>
-				<img class="imgn" src="../img/limpieza.jpg">
-			</div>
-			<div class="imagen">
-				<h4>Cocinera</h4>
-				<li>Direccion: 5 de mayo Oaxaca de Juarez</li>
-				<li>Precio: $7,000 - $8,0000 mensuales</li>
-				<img class="imgn" src="../img/cocinera.jpg">
-			</div>
-			<div class="imagen">
-				<h4>Pintor</h4>
-				<li>Direccion: Teposcolula, Oax</li>
-				<li>Precio: $6,000 - $6,500 mensuales</li>
-				<img class="imgn" src="../img/pintor.jpg">
-			</div>
-			</form>
-		</div>
+<?php include "includes/header.php" ?>
+<br>
+<br>
+<br>
+     
+	<div class="container">
 		
-	</section>
+    <div class="item">
+      <img src="../img/lim.jpg" alt="" class="item-img">
+      <div class="item-text">
+        <h3>Auxiliar de limpieza y mantenimiento</h3>
+        <li>Direccion: Heroica,Cd.de Tlaxiaco</li>
+		  <li>Precio: $5,000 - $5,500 mensuales</li>
+      <input  style="background-color: #FF9900" type="button" value="Mas información">
+
+      </div>
+    </div>
+    
+    <div class="item">
+      <img src="../img/car.jpg" alt="" class="item-img">
+      <div class="item-text">
+        <h3>Carpintero</h3>
+		<li>Dirección: Oaxaca de Juarez, Oax</li>
+		<li>Precio: $10,000 - $12,0000 mensuales</li>
+    <input style="background-color: #FF9900" type="button" value="Mas información">
+      </div>
+    </div>
+    
+    <div class="item">
+      <img src="../img/plo.jpg" alt="" class="item-img">
+      <div class="item-text">
+        <h3>plomero</h3>
+        <li>Direccion:  Oaxaca de Juarez</li>
+		<li>Precio: $7,000 - $8,0000 mensuales</li>
+    <input style="background-color: #FF9900" type="button" value="Mas información">
+      </div>
+    </div>
+    
+    <div class="item">
+      <img src="../img/alb.png" alt="" class="item-img">
+      <div class="item-text">
+        <h3>Albañil</h3>
+		<li>Direccion: centro, Oax</li>
+		<li>Precio: $8,000 - $9,500 mensuales</li>
+    <input style="background-color: #FF9900" type="button" value="Mas información">
+      </div>
+    </div>
+    <div class="item">
+      <img src="../img/coci.jpg" alt="" class="item-img">
+      <div class="item-text">
+        <h3>Cocinera</h3>
+		<li>Direccion: centro, Oax, de Juarez</li>
+		<li>Precio: $7,000 - $8,000 mensuales</li>
+    <input style="background-color: #FF9900" type="button" value="Mas información">
+      </div>
+    </div>
+    
+    <div class="item">
+      <img src="../img/pin.png" alt="" class="item-img">
+      <div class="item-text">
+        <h3>Pintor</h3>
+        <li>Direccion: Teposcolula, Oax</li>
+		<li>Precio: $6,000 - $6,500 mensuales</li>
+    <input style="background-color: #FF9900" type="button" value="Mas información">
+      </div>
+    </div>
+  </div>	
+		
 	<?php include "includes/footer.php" ?>
 </body>
 </html>
