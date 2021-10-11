a39d0dcbf75f3a012273a939fde411613840d221
Eva155 <aguilar19eva@gmail.com>
2021-10-11 13:44:46 -0500

    Footer de la pagina

---
+++ sistema/includes/footer.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Footer Desing</title>
+    <link rel="stylesheet" href="css/estilosfooter.css">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+</head>
+<body>
+    <footer>
+        <div class="container-footer-all">
+            <div class="container-body">
+                <div class="columna1">
+                    <h1>Más información de la pagina</h1>
+                    <p>Prestadores de servicio "EGAL" es una página
+                    web para prestadores de servicios como albañilería,
+                    plomeros, carpinteros, niñeras, cocineras, 
+                    de limpieza etc. Esta plataforma es un medio para poder 
+                    brindarle al cliente el mejor servicio a través de una 
+                    página web para su uso cotidiano, al mismo tiempo ayudar 
+                    a los prestadores de servicio para que puedan conseguir
+                    trabajo de una manera facil, confiable y segura.</p>
+                </div>
+            
+                <div class="columna2">
+                    <h1>Redes sociales</h1>
+                    <div class="row">
+                        <img src="../img/facebook.png">
+                        <label>Siguenos en Facebook</label>
+                    </div>
+                    <div class="row">
+                        <img src="../img/twitter.png">
+                        <label>Siguenos en Twiter</label>
+                    </div>
+                    <div class="row">
+                        <img src="../img/instagram.jpg">
+                        <label>Siguenos en Instagram</label>
+                    </div>
+                </div>
+
+                <div class="Columna3">
+                    <h1>Información contactos</h1>
+                    <div class="row2">
+                        <img src="../img/casa.png">
+                        <label>Heroica Cd. de Tlaxiaco</label>
+                    </div>
+                    <div class="row2">
+                        <img src="../img/whatsapp.png">
+                        <label>9531104223</label>
+                    </div>
+                    <div class="row2">
+                        <img src="../img/gmaill.jpg">
+                        <label>prestadordeservicios@gmail.com</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+       <!-- <div class="container-footer">
+               <div class="footer">
+                    <div class="copyright">
+                        © 2017 Todos los Derechos Reservados | <a href="">EGAL</a>
+                    </div>
+
+                    <div class="information">
+                        <a href="">Informacion Compañia</a> | <a href="">Privacion y Politica</a> | <a href="">Terminos y Condiciones</a>
+                    </div>
+                </div>
+
+            </div>-->
+    </footer>
+</body>
+</html>
