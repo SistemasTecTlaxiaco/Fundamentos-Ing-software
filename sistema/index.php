29c5b2306e08060e8287106386754939a5e60546
Graciela2000 <chequi-2000+@hotmail.com>
2021-10-11 23:07:56 -0500

    subiendo index

--- sistema/index.php
+++ sistema/index.php
@@ -1,7 +1,10 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<?php include "includes/scripts.php" ?>
+<?php include "includes/scripts.php"
+
+?>
 	
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta charset="UTF-8">
@@ -12,74 +15,36 @@
 </head>
 
 <body>
-<?php include "includes/header.php" ?>
+<?php include "includes/header.php"?>
 <br>
 <br>
 <br>
-     
+<br>
+<br>
 	<div class="container">
-		
+      <?php 
+            require_once "../conexion.php";
+      $query = mysqli_query($conection,'SELECT * FROM registro');
+    
+      foreach ($query as $conection){
+      ?>
+    
     <div class="item">
-      <img src="../img/lim.jpg" alt="" class="item-img">
+     <a href="detalle_persona.php?codigo=<?php echo $conection['id_registro'] ?>"> <img src="../img/<?php echo $conection['foto'];?>" class="item-img">
+        <h3><?php echo  $conection['nombreCompleto'];?></h3>
+        <p><?php echo  $conection['tipo_servicio'];?></p>
+		    <a href='detalle_persona.php?codigo=<?php echo $conection['id_registro'] ?>'  class="btn btn-primary" ><center style="background-color: #FF9900">Mas informacion </center> </a>
       <div class="item-text">
-        <h3>Auxiliar de limpieza y mantenimiento</h3>
-        <li>Direccion: Heroica,Cd.de Tlaxiaco</li>
-		  <li>Precio: $5,000 - $5,500 mensuales</li>
-      <input  style="background-color: #FF9900" type="button" value="Mas información">
+    </div>
+    </div>
+      <?php
+      }
+      ?>
 
-      </div>
-    </div>
-    
-    <div class="item">
-      <img src="../img/car.jpg" alt="" class="item-img">
-      <div class="item-text">
-        <h3>Carpintero</h3>
-		<li>Dirección: Oaxaca de Juarez, Oax</li>
-		<li>Precio: $10,000 - $12,0000 mensuales</li>
-    <input style="background-color: #FF9900" type="button" value="Mas información">
-      </div>
-    </div>
-    
-    <div class="item">
-      <img src="../img/plo.jpg" alt="" class="item-img">
-      <div class="item-text">
-        <h3>Plomero</h3>
-        <li>Direccion:  Oaxaca de Juarez</li>
-		<li>Precio: $7,000 - $8,0000 mensuales</li>
-    <input style="background-color: #FF9900" type="button" value="Mas información">
-      </div>
-    </div>
-    
-    <div class="item">
-      <img src="../img/alb.png" alt="" class="item-img">
-      <div class="item-text">
-        <h3>Albañil</h3>
-		<li>Direccion: centro, Oax</li>
-		<li>Precio: $8,000 - $9,500 mensuales</li>
-    <input style="background-color: #FF9900" type="button" value="Mas información">
-      </div>
-    </div>
-    <div class="item">
-      <img src="../img/coci.jpg" alt="" class="item-img">
-      <div class="item-text">
-        <h3>Cocinera</h3>
-		<li>Direccion: centro, Oax, de Juarez</li>
-		<li>Precio: $7,000 - $8,000 mensuales</li>
-    <input style="background-color: #FF9900" type="button" value="Mas información">
-      </div>
-    </div>
-    
-    <div class="item">
-      <img src="../img/pin.png" alt="" class="item-img">
-      <div class="item-text">
-        <h3>Pintor</h3>
-        <li>Direccion: Teposcolula, Oax</li>
-		<li>Precio: $6,000 - $6,500 mensuales</li>
-    <input style="background-color: #FF9900" type="button" value="Mas información">
-      </div>
-    </div>
-  </div>	
-		
+ </div>
+   
 	<?php include "includes/footer.php" ?>
 </body>
-</html>
+</html> 
+
+
